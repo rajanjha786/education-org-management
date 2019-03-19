3b2ad4922102c80c5834acbdfe2e5d7be71331fb
rajan786 <rajanjha786786@gmail.com>
2019-03-19 21:36:25 +0530

    CORS enabled

--- src/main/java/com/codefactory/classmanagement/configuration/AppConfig.java
+++ src/main/java/com/codefactory/classmanagement/configuration/AppConfig.java
@@ -1,11 +1,27 @@
 
 package com.codefactory.classmanagement.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableTransactionManagement
 public class AppConfig {
 
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		
+		return new WebMvcConfigurerAdapter(
+				) {
+			
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
