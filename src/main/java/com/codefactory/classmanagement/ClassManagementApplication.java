ca94a391f289a805c38318ef3d37e1cfe177fec6
rajan786 <rajanjha786786@gmail.com>
2019-03-19 21:38:58 +0530

    Make war of the application

--- src/main/java/com/codefactory/classmanagement/ClassManagementApplication.java
+++ src/main/java/com/codefactory/classmanagement/ClassManagementApplication.java
@@ -5,17 +5,26 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
-public class ClassManagementApplication /*extends SpringBootServletInitializer*/{
+public class ClassManagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClassManagementApplication.class, args);
 		
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(ClassManagementApplication.class);
+	}
+	
+	
 	@Bean
 	@Scope("prototype") 
 	/**
